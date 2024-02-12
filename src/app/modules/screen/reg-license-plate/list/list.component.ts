eae12482909f26b2310f698afefa59eee76a3aba
info1090 <info@asha-tech.co.th>
2024-02-12 07:07:34 +0700

    aaa

--- src/app/modules/screen/reg-license-plate/list/list.component.ts
+++ src/app/modules/screen/reg-license-plate/list/list.component.ts
@@ -170,9 +170,45 @@ export class ListComponent implements OnInit, AfterViewInit {
 
     submit() {
         this._service
-            .getById(this.dataForm.value.license)
+            .getById("2กล-5443")
             .subscribe((resp: any) => {
-              alert(resp);
+                try {
+                    // this.item = resp.data;
+                    if (resp.data) {
+                        const obj = {
+                            data: resp.data,
+                        };
+
+                        // localStorage.setItem('data', JSON.stringify(obj));
+                        // setCookie('data', JSON.stringify(obj))
+                        // this._changeDetectorRef.markForCheck();
+                        this._router.navigate(['screens/reg-kg/list']);
+                    } else {
+                        this._fuseConfirmationService.open({
+                            title: 'เกิดข้อผิดพลาด',
+                            message: 'ไม่พบข้อมูลรถในระบบ กรุณาตรวจสอบข้อมูล',
+                            icon: {
+                                show: true,
+                                name: 'heroicons_outline:exclamation',
+                                color: 'warning',
+                            },
+                            actions: {
+                                confirm: {
+                                    show: false,
+                                    label: 'ตกลง',
+                                    color: 'primary',
+                                },
+                                cancel: {
+                                    show: false,
+                                    label: 'ยกเลิก',
+                                },
+                            },
+                            dismissible: true,
+                        });
+                    }
+                } catch (error) {
+                   alert(error);
+                }
             });
     }
 }
