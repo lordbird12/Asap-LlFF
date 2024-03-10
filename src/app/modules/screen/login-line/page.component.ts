442bff7534d9a960fa388195380e34d62b9e553f
info1090 <info@asha-tech.co.th>
2024-03-11 04:48:14 +0700

    update

--- src/app/modules/screen/login-line/page.component.ts
+++ src/app/modules/screen/login-line/page.component.ts
@@ -90,13 +90,18 @@ export class PageComponent implements OnInit, OnDestroy {
         this.route.queryParams.subscribe((params) => {
             // Access and use query parameters here
             const paramValue = params;
-            const id = paramValue['liff.state'].replace('?template_id=', '');
+            if(paramValue['liff.state']){
+                const id = paramValue['liff.state'].replace('?template_id=', '');
 
-            if (id == '1') {
-                this._router.navigate(['screens/postpon/finish']);
-            } else {
+                if (id == '1') {
+                    this._router.navigate(['screens/postpon/finish']);
+                } else {
+                    this.initLine();
+                }
+            }else{
                 this.initLine();
             }
+           
         });
     }
 
